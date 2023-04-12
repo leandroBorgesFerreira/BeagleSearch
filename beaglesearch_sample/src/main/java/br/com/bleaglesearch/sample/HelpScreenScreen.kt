cf504ce35f714d7a2da9453c49f693cdfc2c3f95
Leandro Borges Ferreira <lehen01@gmail.com>
2023-04-12 21:24:40 +0200

    Simple navigation

---
+++ beaglesearch_sample/src/main/java/br/com/bleaglesearch/sample/HelpScreenScreen.kt
@@ -0,0 +1,24 @@
+package br.com.bleaglesearch.sample
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun HelpScreen(navigateToTransactions: () -> Unit) {
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        Text("HelpScreen", modifier = Modifier.align(alignment = Alignment.Center))
+
+        Button(
+            onClick = navigateToTransactions,
+            modifier = Modifier.align(alignment = Alignment.BottomCenter)
+        ) {
+            Text(text = "Go to Transactions")
+        }
+    }
+}
---
+++ beaglesearch_sample/src/main/java/br/com/bleaglesearch/sample/NavigationActivity.kt
@@ -0,0 +1,45 @@
+package br.com.bleaglesearch.sample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+class NavigationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            NavigationGraph(rememberNavController())
+        }
+    }
+
+    @Composable
+    fun NavigationGraph(navController: NavHostController) {
+        Log.d("NavigationGraph", "creating graph")
+        NavHost(navController = navController, startDestination = Destinations.TRANSACTIONS.id) {
+            composable(Destinations.TRANSACTIONS.id) {
+                Log.d("NavigationGraph", "navigating to transactions")
+                TransactionsScreen(navigateToHelp = {
+                    navController.navigate(Destinations.HELP.id)
+                })
+            }
+
+            composable(Destinations.HELP.id) {
+                Log.d("NavigationGraph", "navigating to help")
+                HelpScreen(navigateToTransactions = {
+                    navController.navigate(Destinations.TRANSACTIONS.id)
+                })
+            }
+        }
+    }
+}
+
+enum class Destinations(val id: String) {
+    TRANSACTIONS("transactions"), HELP("help"),
+}
