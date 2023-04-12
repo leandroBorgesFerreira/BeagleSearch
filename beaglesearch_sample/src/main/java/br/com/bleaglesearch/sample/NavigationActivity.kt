4114731a98723b5ef1b2c2f6b616fc64267cd47c
Leandro Borges Ferreira <lehen01@gmail.com>
2023-04-12 22:09:52 +0200

    Adding bottom bar

--- beaglesearch_sample/src/main/java/br/com/bleaglesearch/sample/NavigationActivity.kt
+++ beaglesearch_sample/src/main/java/br/com/bleaglesearch/sample/NavigationActivity.kt
@@ -1,12 +1,28 @@
 package br.com.bleaglesearch.sample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Help
+import androidx.compose.material.icons.filled.Money
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 
 class NavigationActivity : AppCompatActivity() {
@@ -14,28 +30,72 @@ class NavigationActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            NavigationGraph(rememberNavController())
-        }
-    }
+            val navController = rememberNavController()
 
-    @Composable
-    fun NavigationGraph(navController: NavHostController) {
-        NavHost(navController = navController, startDestination = Destinations.TRANSACTIONS.id) {
-            composable(Destinations.TRANSACTIONS.id) {
-                TransactionsScreen(navigateToHelp = {
-                    navController.navigate(Destinations.HELP.id)
-                })
-            }
+            val items = listOf(
+                Screen.Transactions,
+                Screen.Help,
+            )
 
-            composable(Destinations.HELP.id) {
-                HelpScreen(navigateToTransactions = {
-                    navController.navigate(Destinations.TRANSACTIONS.id)
-                })
+            Scaffold(
+                bottomBar = {
+                    BottomNavigation {
+                        val navBackStackEntry by navController.currentBackStackEntryAsState()
+                        val currentDestination = navBackStackEntry?.destination
+                        items.forEach { screen ->
+                            BottomNavigationItem(
+                                icon = { Icon(screen.icon, contentDescription = null) },
+                                label = { Text(screen.label) },
+                                selected = currentDestination?.hierarchy?.any { navDestination ->
+                                    navDestination.route == screen.route
+                                } == true,
+                                onClick = {
+                                    navController.navigate(screen.route) {
+                                        popUpTo(navController.graph.findStartDestination().id) {
+                                            saveState = true
+                                        }
+                                        launchSingleTop = true
+                                        restoreState = true
+                                    }
+                                }
+                            )
+                        }
+                    }
+                }
+            ) { innerPadding ->
+                NavigationGraph(navController = navController, innerPadding = innerPadding)
             }
         }
     }
 }
 
-enum class Destinations(val id: String) {
-    TRANSACTIONS("transactions"), HELP("help"),
+
+@Composable
+fun NavigationGraph(navController: NavHostController, innerPadding: PaddingValues) {
+    NavHost(
+        navController,
+        startDestination = Screen.Transactions.route,
+        modifier = Modifier.padding(innerPadding)
+    ) {
+        composable(Screen.Transactions.route) {
+            TransactionsScreen(navigateToHelp = {
+                navController.navigate(Screen.Help.route) {
+                    launchSingleTop = true
+                }
+            })
+        }
+
+        composable(Screen.Help.route) {
+            HelpScreen(navigateToTransactions = {
+                navController.navigate(Screen.Transactions.route) {
+                    launchSingleTop = true
+                }
+            })
+        }
+    }
+}
+
+sealed class Screen(val route: String, val label: String, val icon: ImageVector) {
+    object Transactions : Screen("transactions", "transactions", Icons.Default.Money)
+    object Help : Screen("help", "help", Icons.Default.Help)
 }
